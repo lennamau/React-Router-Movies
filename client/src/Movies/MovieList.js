08f52b7ff18739283dbeeb345198b3375f5cf0aa
Lenna <lenna@lennamau.com>
2019-01-22 14:23:58 -0700

    stretch completed

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import { NavLink } from 'react-router-dom'
+import MovieCard from "./MovieCard"
 
 export default class MovieList extends Component {
   constructor(props) {
@@ -37,23 +38,7 @@ function MovieDetails({ movie }) {
   
   return (
     <NavLink to={`/movies/${id}`}>
-
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
+      <MovieCard movie={movie} />
     </NavLink>
   );
 }
